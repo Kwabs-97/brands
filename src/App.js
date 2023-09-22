ec56d68d68fadecc64006a36766a122c9d461aae
Kwabs-97 <khobewayne@gmail.com>
2023-09-22 08:27:51 +0000

    home layout complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 /** @format */
 
+import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 
 function App() {
-  return <div className="App">
-    <Navbar />
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,35 +4,76 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import Button from "../features/Button";
 
+//---- image data -------------//
+import frame1 from "../assets/045a36973b1b54d446d9764f07731d37.png";
+import frame2 from "../assets/2c8931fb85d6d7ed6f8849b21ea4687b.png";
+import eclipse1 from "../assets/33fe647a46f9bf668322f8c1d94ed937.png";
+import eclipse2 from "../assets/7d62b2c9ff8b51ac3a648cf214d14866.png";
+import eclipse3 from "../assets/056cc892636460bee5791aa3f243854c.png";
+import eclipse4 from "../assets/d8003e7c5abaae02be0c524fe88ec378.png";
+import eclipse5 from "../assets/8a0e7a6ab75fc2eabd9a0a9ae72f8ea0.png";
+//---- end of image data -----//
+
 const Home = () => {
   return (
     <div className={styles.home}>
-      <div className={styles.section1}>
-        <header>
+      <div className={styles.container}>
+        <div className={styles.section1}>
+          <header>
+            <section className={styles.topHeading}>
+              <span>TRANSFORMING BRANDS</span>
+            </section>
+            <div className={styles.about}>
+              <section className={styles.mainHeading}>
+                <h2>DRIVE ENGAGEMENT, AND UNLEASH SUCCESS WITH US</h2>
+              </section>
+              <section className={styles.more}>
+                <p>
+                  With a passion for innovation and a commitment to results, we craft tailored
+                  solutions that transform your brand's digital presence.
+                </p>
+              </section>
+            </div>
+          </header>
+          <aside>
+            <Button>Contact Us</Button>
+          </aside>
+        </div>
+        <div className={styles.section2}>
           <section>
-            <span>TRANSFORMING BRANDS</span>
+            <img src={frame1} alt="" className={styles.frame} />
+          </section>
+          <section className={styles.impressionsContainer}>
+            <aside className={styles.impressionsLayout}>
+              <div className={styles.impressions}>
+                <section>
+                  <h3>200+</h3> <p>Happy Clients</p>
+                </section>
+                <section>
+                  <h3>10+</h3> <p>Happy Clients</p>
+                </section>
+              </div>
+              <div className={styles.impressions}>
+                <section>
+                  <h3>20+</h3> <p>Businesses</p>
+                </section>
+                <section>
+                  <h3>20+</h3> <p>Projects</p>
+                </section>
+              </div>
+            </aside>
+            <aside className={styles.eclipseLayout}>
+              <img src={eclipse1} alt="" className={styles.eclipse} />
+              <img src={eclipse2} alt="" className={styles.eclipse} />
+              <img src={eclipse3} alt="" className={styles.eclipse} />
+              <img src={eclipse4} alt="" className={styles.eclipse} />
+              <img src={eclipse5} alt="" className={styles.eclipse} />
+            </aside>
           </section>
           <section>
-            <h2>DRIVE ENGAGEMENT, AND UNLEASH SUCCESS WITH US</h2>
+            <img src={frame2} alt="" className={styles.frame} />
           </section>
-          <section>
-            <p>
-              With a passion for innovation and a commitment to results, we craft tailored solutions
-              that transform your brand's digital presence.
-            </p>
-          </section>
-        </header>
-        <aside>
-          <Button>Contact Us</Button>
-        </aside>
-      </div>
-      <div className={styles.section2}>
-        <section></section>
-        <section>
-          <aside></aside>
-          <aside></aside>
-        </section>
-        <section></section>
+        </div>
       </div>
     </div>
   );

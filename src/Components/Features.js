64fd576c1b7d89d9cf5d9079c5bd4e199b621f34
Kwabs-97 <khobewayne@gmail.com>
2023-09-22 10:00:01 +0000

    features layout

---
+++ src/Components/Features.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import styles from "../styles/Features.module.css";
+
+function Features() {
+  return (
+    <div className={styles.features}>
+      <div className={styles.container}></div>
+    </div>
+  );
+}
+
+export default Features;
